22b2bf7ffd6fdbc3f54f5512f4ec088757742bf7
unknown <lyonya_ivanovskij@mail.ru>
2025-07-24 14:31:39 +0300

    add: content width change handlers

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,9 +4,45 @@ import { Button } from 'src/ui/button';
 import styles from './ArticleParamsForm.module.scss';
 import clsx from 'clsx';
 import { useState, useEffect } from 'react';
+import { Text } from 'src/ui/text';
+import { Select } from 'src/ui/select';
+import {
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	initialSettings: {
+		fontFamilyOption: OptionType;
+		fontSizeOption: OptionType;
+		fontColor: OptionType;
+		contentWidth: OptionType;
+		backgroundColor: OptionType;
+	};
+};
+
+export const ArticleParamsForm = ({
+	initialSettings,
+}: ArticleParamsFormProps) => {
 	const [isOpen, setIsOpen] = useState(false);
+	const [selectedFont, setSelectedFont] = useState(
+		initialSettings.fontFamilyOption
+	);
+	const [selectedSize, setSelectedSize] = useState(
+		initialSettings.fontSizeOption
+	);
+	const [selectedColor, setSelectedColor] = useState(initialSettings.fontColor);
+	const [selectedWidth, setSelectedWidth] = useState(
+		initialSettings.contentWidth
+	);
+	const [selectedBackground, setSelectedBackground] = useState(
+		initialSettings.backgroundColor
+	);
 
 	const toggleSidebar = () => {
 		setIsOpen((prev) => !prev);
@@ -26,6 +62,26 @@ export const ArticleParamsForm = () => {
 		};
 	}, []);
 
+	const handleFontChange = (selected: OptionType) => {
+		setSelectedFont(selected);
+	};
+
+	const handleSizeChange = (selected: OptionType) => {
+		setSelectedSize(selected);
+	};
+
+	const handleBackgroundChange = (selected: OptionType) => {
+		setSelectedBackground(selected);
+	};
+
+	const handleContentWidthChange = (selected: OptionType) => {
+		setSelectedWidth(selected);
+	};
+
+	const handleColorChange = (selected: OptionType) => {
+		setSelectedColor(selected);
+	};
+
 	return (
 		<>
 			<ArrowButton isOpen={isOpen} onClick={toggleSidebar} />
@@ -34,6 +90,46 @@ export const ArticleParamsForm = () => {
 					[styles.container_open]: isOpen,
 				})}>
 				<form className={styles.form}>
+					<Text
+						as={'h2'}
+						size={31}
+						weight={800}
+						fontStyle='normal'
+						uppercase={true}
+						align='left'>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={selectedFont}
+						options={fontFamilyOptions}
+						onChange={handleFontChange}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						selected={selectedSize}
+						options={fontSizeOptions}
+						onChange={handleSizeChange}
+						title='Размер шрифта'
+						name={'Размер шрифта'}
+					/>
+					<Select
+						selected={selectedColor}
+						options={fontColors}
+						onChange={handleColorChange}
+						title='Цвет шрифта'
+					/>
+					<Select
+						selected={selectedBackground}
+						options={backgroundColors}
+						onChange={handleBackgroundChange}
+						title='Цвет фона'
+					/>
+					<Select
+						selected={selectedWidth}
+						options={contentWidthArr}
+						onChange={handleContentWidthChange}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
